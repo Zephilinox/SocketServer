b49a542f96c39d2839275918e42a7463099d1705
Zephilinox <Zephilinox@hotmail.co.uk>
2014-12-06 08:52:34 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SocketServer
+================
+
+Following this video https://www.youtube.com/watch?v=X66hFZG5p3A in order to create a server/client for communications using C# and multi-threading.
---
+++ ServerData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerData
+{
+    public class Class1
+    {
+    }
+}
